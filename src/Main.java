dbf8009bbbfbb0f7ea0793434d6527049c9662b5
Alexandra Schukina <alexandra.kovalenko1999@mail.ru>
2023-12-19 19:05:34 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+        int ticketPrice = 17980;
+        int oneMileRuble = 20;
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+        int miles = ticketPrice / oneMileRuble;
+
+        System.out.println("Начислено миль: " + miles);
+
+    }
+}
